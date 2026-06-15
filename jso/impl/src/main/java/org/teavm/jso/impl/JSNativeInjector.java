ffe15b1d4891476c75bb05110740d9f3e9e1dab2
Martin Hristov <tran4o@gmail.com>
2026-06-15 12:14:53 +0300

    fix JSNativeInjector dataToArrayObject not tot inline code directly

--- jso/impl/src/main/java/org/teavm/jso/impl/JSNativeInjector.java
+++ jso/impl/src/main/java/org/teavm/jso/impl/JSNativeInjector.java
@@ -395,19 +395,13 @@ public class JSNativeInjector implements Injector, DependencyPlugin {
 
     private void dataToArrayObject(InjectorContext context, String className) {
         var writer = context.getWriter();
-        // TODO ADD PROPER MAPPING !
-        String pfx = "(function(a){ for (var i=0;i<a.length;i++) {var v=a[i];if(v===undefined)a[i]=null;"
-                + "else switch(typeof v) {case'string':a[i]=$rt_str(v);break;"
-                + "case'bigint':a[i]=Long_create(v) } } return a })(";
-        String sfx = ")";
-        // TODO ADD PROPER MAPPING !
         writer.appendFunction("$rt_wrapArray").append("(").appendFunction(className).append("(),").ws();
-        writer.append(pfx);
+        writer.append("(function(a){for(var i=0;i<a.length;i++){var v=a[i];if(v===undefined)a[i]=null;"
+                + "else switch(typeof v){case'string':a[i]=")
+                .appendFunction("$rt_str").append("(v);break;case'bigint':a[i]=")
+                .appendFunction("Long_create").append("(v)}}return a})(");
         context.writeExpr(context.getArgument(0), Precedence.min());
-        writer.append(sfx);
-        writer.append(")");
-
-
+        writer.append("))");
     }
 
     @Override
